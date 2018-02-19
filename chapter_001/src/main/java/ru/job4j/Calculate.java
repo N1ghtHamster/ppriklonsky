70ed984bcae087b8a2281e1c646985cff6014350
N1ghtHamster <votymu@yandex.ru>
2018-02-19 15:30:26 +0300

    1.3. JavaDoc. Code convention[#41938]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j;
+
+/**
+* Calculate.
+*
+*@author Pavel Priklonsky (votymu@yandex.ru)
+*/
  
 public class Calculate {
+
+/**
+*Main.
+*@peram args - args.
+*/
 
 public static void main(String[] args){
 	System.out.println("Hello World");
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Pavel Priklonsky(votymu@yandex.ru)
+* @version $Id$
+* @since 19.02.18
+*/
+package ru.job4j;
