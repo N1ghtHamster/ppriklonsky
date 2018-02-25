6be2ad9002a825a69e5f3e4cbace2915f8a4c45b
N1ghtHamster <votymu@yandex.ru>
2018-02-25 20:05:20 +0300

    5.2. Создать программу вычисляющую факториал.[#42663]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,30 @@
+package ru.job4j.loop;
+
+/**
+ * Вычисление факториала.
+ * @author Pavel Priklonsky (votymu@yandex.ru)
+ * @version $Id$
+ * @since 25.02.2018
+ */
+
+public class Board {
+    public String paint(int width, int height) {
+        StringBuilder screen = new StringBuilder();
+        String ln = System.lineSeparator();
+        for (int i = 1; i <= width; ++i) {
+            for (int j = 1; j <= height; ++j) {
+                // условие проверки, что писать пробел или X
+                // Выше в задании мы определили закономерность, когда нужно проставлять X
+                if ((i + j)%2 == 0) {
+                    screen.append("X");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            // добавляем перевод на новую строку.
+            screen.append(ln);
+        }
+        return screen.toString();
+    }
+}
+
