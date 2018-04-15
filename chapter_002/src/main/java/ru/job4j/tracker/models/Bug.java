546b14f7a632ffea025ad279a0ae5bd51d380ee1
N1ghtHamster <votymu@yandex.ru>
2018-04-16 00:55:36 +0300

    Реализация профессий в коде[#46854]

--- chapter_002/src/main/java/ru/job4j/tracker/models/Bug.java
+++ chapter_002/src/main/java/ru/job4j/tracker/models/Bug.java
@@ -2,4 +2,14 @@ package ru.job4j.tracker.models;
 
 public class Bug extends Item {
 
+    /**
+     * Конструктор класса Элемент.
+     *
+     * @param name        Имя элемента
+     * @param description Описание
+     * @param create
+     */
+    public Bug(String name, String description, long create) {
+        super(name, description, create);
+    }
 }
--- chapter_002/src/main/java/ru/job4j/tracker/models/Item.java
+++ chapter_002/src/main/java/ru/job4j/tracker/models/Item.java
@@ -1,31 +1,62 @@
 package ru.job4j.tracker.models;
-
+/**
+ * Программа Заявки.
+ * @author Priklonsky Pavel(votymu@yandex.ru)
+ * @version 0.1
+ */
 public class Item {
+    /**
+     *
+     */
     private String id;
-    public String name;
-    public String description;
-    public long create;
-    public Item() {
+    private String name;
+    private String description;
+    private long create;
+    private Item() {
     }
+    /**
+     * Конструктор класса Элемент.
+     * @param name Имя элемента
+     * @param description Описание
+     * @param create
+     */
     public Item(String name, String description, long create) {
         this.name = name;
         this.description = description;
         this.create = create;
     }
+    /**
+     * Получить имя.
+     * @return
+     */
     public String getName() {
         return this.name;
     }
-
+    /**
+     * Получить
+     * @return
+     */
     public String getDescription() {
         return description;
     }
+    /**
+     *
+     * @return
+     */
     public long getCreate() {
         return this.create;
     }
-
+    /**
+     * Получить Id
+     * @return
+     */
     public String getId() {
         return this.id;
     }
+    /**
+     * Указать Id
+     * @param id
+     */
     public void setId(String id) {
         this.id = id;
     }
