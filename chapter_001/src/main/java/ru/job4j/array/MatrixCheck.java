9dc04866a910318e23245936c0b59f46b15571fd
N1ghtHamster <votymu@yandex.ru>
2018-05-22 18:26:33 +0300

    Merge branch 'master' of github.com:N1ghtHamster/ppriklonsky
    # Conflicts:
    #	chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
    #	chapter_002/src/main/java/ru/job4j/tracker/models/Item.java

---
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -0,0 +1,11 @@
+package ru.job4j.array;
+
+public class MatrixCheck {
+    public boolean mono(boolean[][] data){
+        if data.length == data.
+        boolean result = false;
+        for () {
+        }
+        return result;
+    }
+}
