02e82bff8d24f8a46eedc65c5731a5f528d293b8
N1ghtHamster <votymu@yandex.ru>
2018-05-24 10:44:10 +0300

    5.7. Квадратный массив заполнен true или false по диагонале.[#54299]

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -1,6 +1,18 @@
 package ru.job4j.array;
 
+/**
+ * Проверка, что массиф содержит одинаковые занчения ячеек.
+ * @author Pavel Priklonsky (votymu@yandex.ru)
+ * @version $Id$
+ * @since 24.05.2018
+ */
+
 public class Check {
+    /**
+     * Проверяет. одинаковость содержания ячек массива.
+     * @param data префикс.
+     * @return true или false
+     */
 
     public boolean mono(boolean[] data) {
         boolean result = false;
--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -1,6 +1,19 @@
 package ru.job4j.array;
 
+/**
+ * Проверка, что массиф содержит одинаковые диоганальные занчения ячеек.
+ * @author Pavel Priklonsky (votymu@yandex.ru)
+ * @version $Id$
+ * @since 24.05.2018
+ */
+
 public class MatrixCheck {
+
+    /**
+     * Проверяет. одинаковость содержания диоганальных ячек массива.
+     * @param data префикс.
+     * @return true или false
+     */
     public boolean mono(boolean[][] data) {
         boolean result = false;
         for (int i = 0; i != data.length; i++) {
